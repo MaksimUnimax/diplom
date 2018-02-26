559e8a100e79e0dd53cc6cca52cf7af51b0c6062
Maksim <unymax2014@gmail.com>
2018-02-26 10:10:44 +0300

    123213

---
+++ dz/about.php
@@ -0,0 +1,31 @@
+<?php
+$animals = array(
+    'Africa' => array('Syncerus caffer','Hippopotamus amphibius','Tragelaphus eurycerus Ogilby'),
+    'Australia' => array('Canis lupus dingo','Macropus','Phascolarctos cinereus'),
+    'Asia' => array('Bos javanicus','Arctictis binturong','Bos gaurus'),
+    'Antarctida' => array('Balaenoptera musculus','Hydrurga leptonyx','Mirounga leonina'),
+    'Europa' => array('Sciurus vulgaris','Cervus elaphus Linnaeus','Ursus arctos Linnaeus'),
+);
+
+foreach ($animals as $name => $test) {
+    foreach ($test as $one) {
+        $temp = explode(" ",$one);
+        If (count($temp) == 2) {$first[$name][] = $temp[0];}
+        If (count($temp) == 2) {$second[] = $temp[1];}   
+    }
+}
+shuffle($second);
+foreach($first as $key3 => $val){
+    foreach ($val as $val2){
+        $arr[$key3][] = $val2 . " " . $second[$count2];
+        $count2 = $count2 + 1;
+    }
+}
+foreach ($arr as $key4 => $value) {
+	echo "<p>";
+	echo "<strong>", $key4,"</strong>", "<br/>";
+	foreach ($value as  $value2) {
+		echo $value2, "<br/>";
+	}
+};
+?>
