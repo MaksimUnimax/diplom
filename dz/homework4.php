d8554f805c4d63c3b9b0bccb0f27818e4c16cd63
Maksim <unymax2014@gmail.com>
2018-03-06 13:55:34 +0300

    Домашнее задание4

--- dz/homework4.php
+++ dz/homework4.php
@@ -1,5 +1,10 @@
 <?php 	
 
 $test = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=London&appid=ffbe66814f71916c07738ff15f6e694f');
-$test2 =json_decode($test, true);
-var_dump($test);
+//var_dump($test2);
+$test2 = json_decode($test, true);
+foreach ($test2 as $key => $value) {
+	if ($key = 'temp') {$temp = $value;}
+}
+var_dump($test2);
+var_dump($temp);
