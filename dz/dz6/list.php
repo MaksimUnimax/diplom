99e4c90799d0dbd3a68dd732a3d7a42231f4ff8b
Maksim <unymax2014@gmail.com>
2018-03-14 10:54:59 +0300

    Добавил ошибку 404 для 7 задания

--- dz/dz6/list.php
+++ dz/dz6/list.php
@@ -1,7 +1,7 @@
 <?php
 $dir = 'tests';
 $list = scandir($dir);
-$i = 0;
+
 foreach ($list as $key => $value) {
 	echo ++$i . ' ' . $value . '<br/>';
 }
--- dz/dz6/test.php
+++ dz/dz6/test.php
@@ -1,51 +1,58 @@
-<?php 	
+
+<html>
+  <h2>Введите номер теста.</h2>
+  <form action="test.php" method="GET" >
+    <input type="text" name="number" />
+    <input type="submit" value="Отправить" />
+  </form>
+
+<?php   
  $dir = 'tests';
  $list = scandir($dir);
  foreach ($list as $i => $value) {
- 	if ($i > 1){
- 	$fileadr = "$dir/$value";
- 	$filejs1 = file_get_contents("$fileadr");
- 	$filejs = json_decode($filejs1, 'true');
- 		 foreach ($filejs[0] as $key => $numberjs) {
- 		 	 if ($_GET["number"] == $numberjs) {
- 		 	 	$true = $filejs[0]["answerthree"];
-				$chek = $_post['answer'][0];
-				$ques = $filejs[0]["question"];
-				$answ1 = $filejs[0]["answerone"];
-				$answ2 = $filejs[0]["answertwo"];
-				$answ3 = $filejs[0]["answerthree"][0];
- 		 	 	echo "<div> <h2>Тест</h2>
-						<p>$ques</p>
-						<form action='test.php' method='Post'>
-							<select multiple name='answer'>
-								<option value='$answ1'>$answ1</option>
-								<option value='$answ2'>$answ2</option>
-								<option value='$true'>$answ3</option>
-							</select>
-							<input type='submit' value='Ответ' />
-						</form>	
-					</div>";
+    if ($i > 1){
+    $fileadr = "$dir/$value";
+    $filejs1 = file_get_contents("$fileadr");
+    $filejs = json_decode($filejs1, 'true');
+       // foreach ($filejs[0] as $key => $numberjs) {
+        // if ($_GET["number"] != $filejs[0]["number"]){
+        //   http_response_code(404);
+        //   echo '123';
+        //  }
+          if ($_GET["number"] == $filejs[0]["number"]) {
+          $true = $filejs[0]["answerthree"];
+          $ques = $filejs[0]["question"];
+          $answ1 = $filejs[0]["answerone"];
+          $answ2 = $filejs[0]["answertwo"];
+          $answ3 = $filejs[0]["answerthree"][0];
+          echo "<div> <h2>Тест</h2>
+              <p>$ques</p>
+              <form action='test.php' method='Post'>
+                <select multiple name='answer'>
+                  <option value='$answ1'>$answ1</option>
+                  <option value='$answ2'>$answ2</option>
+                  <option value='$true'>$answ3</option>
+                </select>
+                <input type='submit' value='Ответ' />
+              </form> 
+            </div>";
+           
+          }
+    } 
+}
+  if ($_GET["number"] > $filejs[0]["number"] or $_GET["number"]  == 0){
+              http_response_code(404);
+              echo 'Ошибка 404' . '<br/>' . 'Тест не найден';
+  }  
+  
 
- 		 	 }
- 		 }
- 	}	
- }
- ?>
+  if ($_POST['answer'] == 'Array') {
+    echo 'Верно';
+  }
 
-<html>
-	<h2>Введите номер теста.</h2>
-	<form action="test.php" method="GET" >
-		<input type="text" name="number" />
-		<input type="submit" value="Отправить" />
-	</form>
-
-<?php
-	if ($_POST['answer'] == 'Array') {
-		echo 'Верно';
-	}
-	elseif (is_string($_POST['answer'])){
-			echo 'Неверно';
-	}
+  elseif (is_string($_POST['answer'])){
+      echo 'Неверно';
+  }
 ?>
-	
+  
 </html>
