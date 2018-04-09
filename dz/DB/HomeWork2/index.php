68644b3d97b9fec288177a8dab01d2e2c0fac6ba
Your NameMaksgit add dz123 git config --global user.name Your <you@example.com>
2018-04-09 16:44:53 +0500

    Дз

--- dz/DB/HomeWork2/index.php
+++ dz/DB/HomeWork2/index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $dbname = "HomeWork";
 $login = "root";
 $pass = "";
@@ -7,15 +7,21 @@ $about = $_POST['about'];
 $id = $_GET['id'];
 $doneUpd = $row['is_done'];
 $done = 'Не выполнено'; 
+$sell = "Отправить";
+$postName = "about";
 $dataBase = new PDO("mysql:host=localhost;dbname=$dbname", $login, $pass);
 $sql = "INSERT INTO `tasks`( `description`, `is_done`) VALUES ('$about',0)";
 $sql1 = "SELECT * FROM `tasks`";
-$del = "DELETE * FROM `tasks` WHERE id like '$id'";
+$del = "DELETE FROM `tasks` WHERE id like '$id'";
 $update = "UPDATE `tasks` SET is_done=1 where id like '$id'";
-$dataBase->exec($sql);
-var_dump($about);
-var_dump($_GET);
-
+$updAll = "UPDATE `tasks` SET description='$_POST[descrip]' where id like '$_SESSION[id]'";
+$updSel = "SELECT description FROM `tasks` where id like '$id'";
+if (isset($about) and $about != "") {
+	$dataBase->exec($sql);
+	header("location: index.php");	
+}
+// var_dump($descrip);
+// var_dump($_POST);
 ?>
 
 <!Doctype html>
@@ -31,9 +37,20 @@ var_dump($_GET);
 <body>
 	<h1>Список дел</h1>
 	<div style="float: left">
+<?php
+		if ($_GET['action'] == 3) {
+			foreach($dataBase->query($updSel) as $rowOne){
+			$descrip = $rowOne;
+			}
+			$sell = "Сохранить";
+			$postName = "descrip";
+			$_SESSION['id'] = $id;
+			$_SESSION['descrip'] = $descrip[0];
+		}
+?>
 		<form action="index.php" method="POST">
-			<input type="text" name="about" plaseholder= "Описание задачи">
-			<input type="submit" value="Добавить">
+			<input type="text" name=<?php echo $postName; ?> plaseholder= "Описание задачи" value=<?php echo $descrip[0]; ?> >
+			<input type="submit" value=<?= $sell?>>
 		</form>
 	</div>
 	<div style="float: left; margin-left: 20px;">
@@ -41,7 +58,7 @@ var_dump($_GET);
 			<select name="sort">
 				<option value="date">Дата добавления</option>
 				<option value="done">Статус</option>
-				<option value="description">Описанию</option>
+				<option value="description">Описание</option>
 			</select>
 			<input type="submit" value="Отсортировать">
 		</form>
@@ -56,9 +73,19 @@ var_dump($_GET);
 				<th></th>
 			</tr>
 <?php 				
-
-					if ($_GET['action']) {
+var_dump($_POST['descrip']);echo "<br/>";
+var_dump($descrip);echo "<br/>";
+var_dump($descrip[0]);echo "<br/>";
+var_dump($_SESSION['id']);echo "<br/>";
+var_dump($_SESSION['descrip']);echo "<br/>";
+					if (isset($_POST['descrip'])) {
+						$dataBase->exec($updAll);
+					}
+					if ($_GET['action'] == 1) {
 				    	$dataBase->exec($update);
+				    }
+				    if ($_GET['action'] == 2) {
+				    	$dataBase->exec($del);
 				    }
 				    foreach($dataBase->query($sql1) as $row) {
 				    	if($row['is_done'] == 1) {
@@ -66,17 +93,15 @@ var_dump($_GET);
 				    	}else {
 				    		$done = 'Не выполнено';
 				    	}
+				   
 				    	 echo "<tr>
 				 		 <td>$row[description]</td>
 				 		 <td>$row[date_added]</td>
 				 		 <td>$done</td>
-				 		 <td><a href=?id=$row[id]&action=1>Выполнить</a></td>
-				 		 </tr>";
-				 		
-					} 
-
-
-?>					
+				 		 <td><a href=?id=$row[id]&action=1>Выполнить</a> <a href=?id=$row[id]&action=2>Удалить</a> <a href=?id=$row[id]&action=3>Редактировать</a></td>
+				 		 </tr>";				 		 				 		
+					}
+?>
 		</table>
 	</div>
 </body>
