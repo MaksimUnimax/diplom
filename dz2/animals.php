458d2c75023bc66cdbe629152cc654c476116604
Maksim <unymax2014@gmail.com>
2018-08-17 07:51:14 +0300

    Домашнее задание

---
+++ dz2/animals.php
@@ -0,0 +1,28 @@
+<?php
+$animals = array(
+    'Africa' => array('Syncerus caffer','Hippopotamus amphibius','Tragelaphus eurycerus Ogilby'),
+    'Australia' => array('Canis lupus dingo','Macropus','Phascolarctos cinereus'),
+    'Asia' => array('Bos javanicus','Arctictis binturong','Bos gaurus'),
+    'Antarctida' => array('Balaenoptera musculus','Hydrurga leptonyx','Mirounga leonina'),
+    'Europa' => array('Sciurus vulgaris','Cervus elaphus Linnaeus','Ursus arctos Linnaeus'),
+);
+foreach ($animals as $name => $test) {
+    foreach ($test as $one) {
+        $temp = explode(" ",$one);
+        If (count($temp) == 2) {
+             $first[$name][] = $temp[0];
+             $second[] = $temp[1];}   
+    }
+}
+shuffle($second);
+foreach($first as $key3 => $val){
+    foreach ($val as $val2){
+        $arr[$key3][] = $val2 . " " . array_shift($second);
+    }
+}
+foreach ($arr as $key4 => $value) {
+	echo "<p>";
+	echo "<strong>", $key4,"</strong>", "<br/>";
+	echo implode(", ",$value);
+    };
+?>
