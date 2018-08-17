27589c1783657ef6051b251151ec234ce24053fb
Maksim <unymax2014@gmail.com>
2018-08-17 06:48:24 +0300

    Домашнее задание

--- dz2/about.php
+++ dz2/about.php
@@ -1,1 +1,36 @@
-123
+<?
+$name = Maksim;
+$secName = Yagofarov;
+$age = 31;
+$adress = Chelyabinsk;
+$mail = "unymax2014@gmail.com";
+$about = "Little man in big world";
+?>
+
+<!DOCTYPE>
+<html>
+	<head>
+		<title>About</title>
+	</head>
+	<body>
+		<h1>About</h1>
+		<table border='1', cellpadding = '5', cellspacing = '0'>
+			<tr>
+				<td>Name</td>
+				<td>SecName</td>
+				<td>age</td>
+				<td>adress</td>
+				<td>mail</td>
+				<td>about</td>
+			</tr>
+				<tr>
+				<td><?=$name?></td>
+				<td><?=$secName?></td>
+				<td><?=$age?></td>
+				<td><?=$adress?></td>
+				<td><?=$mail?></td>
+				<td><?=$about?></td>
+			</tr>
+		</table>
+	</body>
+</html>
