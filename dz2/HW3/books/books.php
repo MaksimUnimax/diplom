0a564245640202d6f2f0337c4a923cb4c2b2f3e6
Maksim <unymax2014@gmail.com>
2018-08-19 20:18:39 +0300

    Домашнее задание

--- dz2/HW3/books/books.php
+++ dz2/HW3/books/books.php
@@ -4,35 +4,7 @@ $qwery = implode(" ", array_slice($argv, 1));
 $qwery_str = urlencode($qwery);
 $file_ad = "books.csv";
 $file = file_get_contents("https://www.googleapis.com/books/v1/volumes?q=$qwery_str");
-foreach ($file as $files) {
-    json_decode($files);
-    switch (json_last_error()) {
-        case JSON_ERROR_NONE:
-            echo ' - Ошибок нет';
-        break;
-        case JSON_ERROR_DEPTH:
-            echo ' - Достигнута максимальная глубина стека';
-        break;
-        case JSON_ERROR_STATE_MISMATCH:
-            echo ' - Некорректные разряды или несоответствие режимов';
-        break;
-        case JSON_ERROR_CTRL_CHAR:
-            echo ' - Некорректный управляющий символ';
-        break;
-        case JSON_ERROR_SYNTAX:
-            echo ' - Синтаксическая ошибка, некорректный JSON';
-        break;
-        case JSON_ERROR_UTF8:
-            echo ' - Некорректные символы UTF-8, возможно неверно закодирован';
-        break;
-        default:
-            echo ' - Неизвестная ошибка';
-        break;
-    }
-
-    echo PHP_EOL;
-}
-//$files = json_decode($file, true);
+$files = json_decode($file, true);
 $res = fopen($file_ad, "w+");
  foreach ($files["items"] as $key => $value) {
  	$id += 1;
@@ -40,9 +12,5 @@ $res = fopen($file_ad, "w+");
  	$author = $value["volumeInfo"]["authors"][0];
 	fwrite($res, $id . "," . $title . ",". $author . "\n");
  }
-
  	echo file_get_contents($file_ad);
-
-
-
 ?>
