3924c21ec3c6ec1bc52a7d1d634530fa43bc3f45
Maksim <unymax2014@gmail.com>
2018-08-21 17:53:16 +0300

    Домашнее задание

--- dz2/HW4/index.php
+++ dz2/HW4/index.php
@@ -1,10 +1,47 @@
 <?
 
-$file_ad = "json.json";
+$file_ad = "test1.json";
 $file_js = file_get_contents("$file_ad");
 $file = json_decode($file_js, true);
-var_dump($file);
-foreach ($file_js as $key => $value) {
-	# code...
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Table</title>
+	</head>
+	<body>
+		<table>
+			<?
+			foreach ($file as $key => $value) { 
+			?>
+				<tr>
+					<? 
+						echo "<td>" ."Имя". "</td>";
+						echo "<td>" . $value["firstName"] . "</td>";
+					?>
+				</tr>
+				<tr>
+					<? 
+						echo "<td>" ."Фамилия". "</td>";
+						echo "<td>" . $value["lastName"] . "</td>";
+					?>
+				</tr>
+				<tr>
+					<? 
+						echo "<td>" ."Адрес". "</td>";
+						echo "<td>" . $value["address"] . "</td>";
+					?>
+				</tr>
+				<tr>
+					<? 
+						echo "<td>" ."Телефон". "</td>";
+						echo "<td>" . $value["phoneNumber"] . "</td>";
+					?>
+				</tr>
+		</table>
+	</body>
+</html>
+<?
 }
 ?>
