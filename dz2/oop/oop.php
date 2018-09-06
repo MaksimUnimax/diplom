01ad8caf737f0165da8ac6d60ab6ebc67d03d36a
Maksim <unymax2014@gmail.com>
2018-09-06 09:43:46 +0300

    Домашнее задание

--- dz2/oop/oop.php
+++ dz2/oop/oop.php
@@ -1,11 +1,10 @@
 <?php 
-
+$count = 0;
 abstract class LiveInEarth
 {
 	protected $name;
 	public $clasification;
 	public $nameScientific;
-	protected $ageEarth;
 
 	public function __construct($name,$clasification,$nameScientific)
 	{
@@ -16,10 +15,10 @@ abstract class LiveInEarth
 
 	public function isItReasonable()
 	{
-			if($this -> nameScientific = "HomoSapiens") {
-			echo "Разумный";
+			if($this -> nameScientific == "HomoSapiens") {
+			return "Разумный";
 		}else {
-			echo "Неразумный";	
+			return "Неразумный";	
 		}
 	}
 
@@ -30,7 +29,7 @@ class Human extends LiveInEarth
 {
 	public function getName()
 	{
-		echo $this -> name;
+		return $this -> name;
 	}
 }
 
@@ -38,7 +37,7 @@ class Cow extends LiveInEarth
 {
 	public function getName()
 	{
-		echo $this -> name;
+		return $this -> name;
 	}	
 }
 
@@ -46,7 +45,7 @@ class Spider extends LiveInEarth
 {
 	public function getName()
 	{
-		echo $this -> name;
+		return $this -> name;
 	}
 }
 
@@ -54,7 +53,7 @@ class Dolphin extends LiveInEarth
 {
 	public function getName()
 	{
-		echo $this -> name;
+		return $this -> name;
 	}
 }
 
@@ -62,10 +61,45 @@ class Eagle extends LiveInEarth
 {
 	public function getName()
 	{
-		echo $this -> name;
+		return $this -> name;
 	}	
 }
 $BrachypelmaSmithi = new Spider("Паук-птицеед","Членистоногий","Theraphosidae");
 $human = new Human("Человек","Млекопитающее","HomoSapiens");
-$
+$bosTaurus = new Cow("Голштино-фризская корова","Млекопитающее","Bos taurus");
+$tursiopsTruncatus = new Dolphin("Афалин","Млекопитающее","Tursiops truncatus");
+$aquilaChrysaetos = new Eagle("Беркут","Птицы","Aquila chrysaetos");
+$live = [$BrachypelmaSmithi, $human, $bosTaurus, $tursiopsTruncatus, $aquilaChrysaetos];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Live in Earth</title>
+</head>
+<body>
+	<table border="1">
+		<tr>
+			<th>Название</th>
+			<th>Класс</th>
+			<th>Научное название</th>
+			<th>Разумность</th>
+		</tr>
+		<tr>
+			<?php 
+			foreach ($live as $value) {
+				$count += 1;
+			?>
+			<td><?php echo $count . ") " . $value -> getName()?></td>
+			<td><?php echo $value -> clasification?></td>
+			<td><?php echo $value -> nameScientific?></td>
+			<td><?php echo $value -> isItReasonable()?></td>
+			
+		</tr>
+		<?php
+			}
+		?>
+	</table>
+</body>
+</html>
